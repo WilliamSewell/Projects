a1bbcc6b991ec54e69cc1c6a0c7fa60a682540a6
William Sewell <will.sewell@woodstarsoftware.co.uk>
2025-12-11 12:05:06 +0000

    Filled out pages

---
+++ WilliamSewellPortfolio/WilliamSewellPortfolio/Pages/Interests/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WilliamSewellPortfolio.Pages.Interests
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WilliamSewellPortfolio/WilliamSewellPortfolio/Pages/Philosophy/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WilliamSewellPortfolio.Pages.Philosophy
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
